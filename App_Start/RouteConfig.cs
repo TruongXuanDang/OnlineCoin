3883818c1a89f99a57f113710bedae1c84c2ea03
TruongDX8 <truonghusk17@gmail.com>
2020-01-13 19:36:24 +0700

    Change Shared Layout

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace OnlineCoin
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Coins", action = "Index", id = UrlParameter.Optional }
             );
         }
     }

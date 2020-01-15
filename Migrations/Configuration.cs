dc3d05513c8ad54b4fb02ca50359751065b88738
TruongDX8 <truonghusk17@gmail.com>
2020-01-15 20:43:52 +0700

    Configuration file to mock data

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -1,12 +1,19 @@
 namespace OnlineCoin.Migrations
 {
+    using OnlineCoin.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<OnlineCoin.Models.OnlineCoinContext>
     {
+        List<Account> listAccount = new List<Account>();
+        List<AccountRole> listAccountRole = new List<AccountRole>();
+        List<Coin> listCoin = new List<Coin>();
+        List<News> listNews = new List<News>();
+
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
@@ -14,18 +21,54 @@ namespace OnlineCoin.Migrations
 
         protected override void Seed(OnlineCoin.Models.OnlineCoinContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            AddAccount("Tran", "An");
+            AddAccount("Tran", "Binh");
+            AddAccount("Tran", "Tuan");
+            AddAccount("Nguyen", "Can");
+            AddAccount("Dang", "Trung");
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            AddAccountRole("User");
+            AddAccountRole("Admin");
+
+            AddNews("Maintain your Finance and Technology Expertise", "Join us at our events to watch pitches by companies deploying the latest technology, watch our video library of over 400 videos, and follow our newsletters with valuable information on the latest reports and happenings in the scene.");
+
+            context.Accounts.AddRange(listAccount);
+            context.AccountRoles.AddRange(listAccountRole);
+            context.News.AddRange(listNews);
+            context.SaveChanges();
+        }
+
+        private void AddAccount (string firstName, string lastName)
+        {
+            listAccount.Add(new Account
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                CreatedAt = DateTime.Now,
+                Status = Account.AccountStatus.Active,
+
+            });
+        }
+
+        private void AddAccountRole (string description)
+        {
+            listAccountRole.Add(new AccountRole
+            {
+                Description = description,
+                CreatedAt = DateTime.Now,
+                Status = AccountRole.AccountRoleStatus.Active,
+            });
+        }
+
+        private void AddNews (string title, string content)
+        {
+            listNews.Add(new News
+            {
+                Title = title,
+                Content = content,
+                PublishedAt = DateTime.Now,
+                Status = News.AuthorStatus.Active,
+            });
         }
     }
 }
